2b317a49a3d228b3264ff2c023b7cb9d5c4d7a85
yang <joker_yang@foxmail.com>
2023-12-15 16:53:34 +0800

    Update gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -5,7 +5,7 @@ function deleteListAd(list){
   let tempList = []
   for (let i in list) {
     let temp = list[i]
-    if(temp.adId == "0")
+    if(temp.adId == "0" && temp.childElements == null)
     {
       tempList.push(temp)
     }
