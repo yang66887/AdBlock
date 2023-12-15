8178d0a85b241bf42c3a7a5e5d251bff96d21bab
yang <joker_yang@foxmail.com>
2023-12-15 23:07:54 +0800

    Update gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -1,19 +1,31 @@
 var body = JSON.parse($response.body);
 var listElements = body.listElements;
+var channels = body.channels
 
-for (let i in listElements) {
-  let temp = listElements[i]
-  if(!temp.adId){
-    listElements.splice(i, 1)
+if(listElements != ""){
+  for (let i in listElements) {
+    let temp = listElements[i]
+    if(!temp.adId){
+      listElements.splice(i, 1)
+    }
+    if(temp.adId != "0"){
+      listElements.splice(i, 1)
+    }
+    if(temp.title == "EPIC游戏商城假日特卖"){
+      listElements.splice(i, 1)
+    }
+    if(temp.title == "推荐专题"){
+      listElements.splice(i, 1)
+    }
   }
-  if(temp.adId != "0"){
-    listElements.splice(i, 1)
-  }
-  if(temp.title == "EPIC游戏商城假日特卖"){
-    listElements.splice(i, 1)
-  }
-  if(temp.title == "推荐专题"){
-    listElements.splice(i, 1)
+}
+
+if(channels != ""){
+  for(let y in channels) {
+    let channel = channels[y]
+    if(channel.channelId != 2 && channel.channelId != 9 && channel.channelId != 11 && channel.channelId != 19){
+      channels.splice(y, 1)
+    }
   }
 }
 
