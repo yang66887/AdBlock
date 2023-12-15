3f6db6b46dde9bf66b5d5ae398176121ff7d2fba
yang <joker_yang@foxmail.com>
2023-12-15 18:07:17 +0800

    Update gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -12,6 +12,9 @@ for (let i in listElements) {
   if(temp.title == "EPIC游戏商城假日特卖"){
     listElements.splice(i, 1)
   }
+  if(temp.title == "推荐专题"){
+    listElements.splice(i, 1)
+  }
 }
 
 $done({body: JSON.stringify(body)});
