98046716e811944ae776130634f0039ada32494b
yang <joker_yang@foxmail.com>
2023-12-18 12:02:28 +0800

    Update gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -2,8 +2,8 @@ var body = JSON.parse($response.body);
 var listElements = body.listElements;
 var channels = body.channels
 
-if(listElements != ""){
-  for (let i in listElements) {
+if(listElements){
+  for (let i = listElements.length - 1; i >= 0; i--) {
     let temp = listElements[i]
 //    if(!temp.adId && temp.childElements){
 //      let childElements = temp.childElements
@@ -26,8 +26,8 @@ if(listElements != ""){
   }
 }
 
-if(channels != ""){
-  for(let y in channels) {
+if(channels){
+  for(let y = channels.length - 1; y >= 0; y--) {
     let channel = channels[y]
     if(channel.channelId != 2 && channel.channelId != 9 && channel.channelId != 11 && channel.channelId != 19){
       channels.splice(y, 1)
