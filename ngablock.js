71b4db7d74f272e5ad05f1c63ae303a311ef1087
yang <joker_yang@foxmail.com>
2023-12-19 10:22:53 +0800

    Update and rename ngaBlock.js to ngablock.js

--- ngablock.js
+++ ngablock.js
@@ -1,23 +1,33 @@
 var body = JSON.parse($response.body);
 var result = body.result;
 var data = result.data;
+
+# 帖子主题黑名单
 const subjectExp = /董宇辉|南方.*北方.*冷|华为/;
+
+# 子版块黑名单
 const forumnameExp = /二手交易|NGATOYS|优惠信息.*|影音讨论区|小窗视界|青玉巫婆|游戏综合|生命之杯|汽车俱乐部|篮球|萌萌宠物|狼人杀|娱乐吃瓜|晴风村|桌游讨论|剧本杀|永歌森林|国际新闻|视频与主播|鹅鸭杀/;
+
+# 用户黑名单
 const authorExp = /背锅师哥|洪逸超自行宫刑|小猪佩奇与乔治|jx372225411|绝无此种可能|a3844169|liu27a2|问很多问题|PookaPooka|Linkle123|你可能小赚我永远不亏|w17640330020|日炎之星索拉卡|fy3211895|快乐小啊啊|萌萌哒苹果|白月光萧容鱼|苏门答腊群岛|黄河鬼棺急|Zhalsan|河南猎手|接济阶级|hotsava33|芽吹|各凭本事吧|蚂蚁哥哥牛牛牛|ailcx|你说你上班吧几年级|试试雾岛|猜火鸡|永不复归o|alexwd|心之所向心之所往|Kopitee|新手流量密码|无昼无夜无边|lora棒球侠|com4911edu|醉清风（求撩）|学材料的单身汪|那你等吧呢好吧|锌锌坐提|wvmwg|比格沃斯233|法斯菲鲁|光闇萌抚子|兰亭集势/
 
+# 主题帖过滤
 if(data){
   for(let i = data.length - 1; i >= 0; i--){
     let subject = data[i].subject
     let forumname = data[i].forumname
     let author = data[i].author
+    # 屏蔽黑名单主题帖
     if(subject.match(subjectExp)){
       data.splice(i, 1)
       continue
     }
+    # 屏蔽黑名单子版块
     if(forumname.match(forumnameExp)){
       data.splice(i, 1)
       continue
     }
+    # 屏蔽黑名单用户发布的主题帖
     if(author.match(authorExp)){
       data.splice(i, 1)
       continue
@@ -25,9 +35,11 @@ if(data){
   }
 }
 
+# 楼层过滤
 if(body.tsubject){
   for(let y = result.length - 1; y >= 0; y--){
     let username = result[y].author.username
+    # 屏蔽黑名单用户楼层
     if(username.match(authorExp)){
       result.splice(y, 1)
       continue
