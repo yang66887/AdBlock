673b5573a94b6d3744f477697577fe8f8213f99c
yang <joker_yang@foxmail.com>
2023-12-19 16:31:46 +0800

    Update gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -5,9 +5,9 @@ var channels = body.channels
 if(listElements){
   for (let i = listElements.length - 1; i >= 0; i--) {
     let temp = listElements[i]
-    if(!temp.adId){
-      listElements.splice(i, 1)
-    }
+    //if(!temp.adId){
+    //  listElements.splice(i, 1)
+    //}
     if(temp.adId != "0"){
       listElements.splice(i, 1)
     }
