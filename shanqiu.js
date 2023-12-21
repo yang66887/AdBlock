547bf3c44ca62a4f1b19d38e7d5fad7282af8801
yang <joker_yang@foxmail.com>
2023-12-21 18:45:39 +0800

    Update shanqiu.js

--- shanqiu.js
+++ shanqiu.js
@@ -2,10 +2,9 @@ var requestUrl = $request.url;
 var resetApi = /resetvip/;
 var infoApi = /myinfo/;
 var addApi = /addsecond/;
-var scanApi = /scantrans/;
 var requestParams = requestUrl.split('?')[1].split('&');
 var params = {};
-var temp = {"uuid":null,"banned":"1","headimgurl":null,"type":null,"wxunionid":null,"token":null,"vipto":null,"wxopenid":null,"nickname":null,"email":null,"appleid":null,"device":null}
+var temp = {"uuid":null,"banned":"9","headimgurl":null,"type":null,"wxunionid":null,"token":null,"vipto":null,"wxopenid":null,"nickname":null,"email":null,"appleid":null,"device":null}
 
 for(let i in requestParams){
   let paramKey = requestParams[i].split('=')[0]
@@ -21,6 +20,8 @@ temp.type = "9"
 temp.vipto = "2099-01-01 00:00:00"
 temp.email = "for_example@gmail.com"
 temp.appleid = "for_example@gmail.com"
+temp.wxunionid = "原神大王"
+temp.nickname = "原神大王"
 temp.token = params.token
 temp.device = params.device
 temp.uuid = params.uid
@@ -33,6 +34,8 @@ if(requestUrl.match(resetApi) || requestUrl.match(addApi)){
   body.token = params.token
   body.email = temp.email
   body.appleid = temp.appleid
+  body.wxunionid = temp.wxunionid
+  body.nickname = temp.nickname
   body.device = params.device
   body.uuid = params.uid
   $done({body: JSON.stringify(body)});
@@ -43,22 +46,4 @@ if(requestUrl.match(infoApi)){
   body.status = "1"
   body.data[0] = temp
   $done({body: JSON.stringify(body)});
-}
-
-if(requestUrl.match(scanApi)){
-  var requestBody = $request.body;
-  var responseBody = JSON.parse($response.body);
-  if(requestBody){
-    var cid = requestBody.split('&')[1].split('=')[1]
-    requestBody = "fromuid=7aa16c9b8&cid=" + cid
-    $done({body: console.log(requestBody)});
-  }
-  if(responseBody){
-    responseBody.status = "1"
-    responseBody.data = "转移成功"
-    responseBody.type = "9"
-    responseBody.token = params.token
-    responseBody.device = params.device
-    $done({body: JSON.stringify(responseBody)});
-  }
 }
