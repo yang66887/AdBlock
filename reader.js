8d18ac1f8aad67a5a36c6d7b5d0cf9dc18b8e221
yang <joker_yang@foxmail.com>
2024-01-04 10:49:22 +0800

    Update reader.js

--- reader.js
+++ reader.js
@@ -2,4 +2,10 @@ var headers = $request.headers
 
 headers['X-Observer-Mode-Enabled'] = 'true'
 
+headers['X-Client-Build-Version'] = '0'
+
+headers['X-Client-Version'] = '2.0.1'
+
+//headers[''] = 
+
 $done({headers})
