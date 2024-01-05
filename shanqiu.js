1dc5cdb45e378b1499b939deff6735dc88221b4b
yang <joker_yang@foxmail.com>
2024-01-05 09:26:17 +0800

    Update shanqiu.js

--- shanqiu.js
+++ shanqiu.js
@@ -16,11 +16,12 @@ for(let i in requestParams){
   params[paramKey] = paramValue
 }
 //预设vip数据
-var temp = {"uuid":null,"banned":"9","headimgurl":null,"type":null,"wxunionid":null,"token":null,"vipto":null,"wxopenid":null,"nickname":null,"email":null,"appleid":null,"device":null}
+var temp = {"uuid":null,"banned":"0","headimgurl":null,"type":null,"wxunionid":null,"token":null,"vipto":null,"wxopenid":null,"nickname":null,"email":null,"appleid":null,"device":null}
 temp.type = "9" //普通会员：0-7，无法绑定Apple ID | 年费会员：8 | 终身会员：9 | 普通会员：10+，可绑定Apple ID
 temp.vipto = "2099-01-01 00:00:00"
 temp.email = "for_example@gmail.com"
 temp.appleid = "for_example@gmail.com"
+temp.nickname = "咸蛋超人"
 temp.token = params.token
 temp.device = params.device
 temp.uuid = params.uid
@@ -33,17 +34,18 @@ if(requestUrl.match(infoApi)){
 if(requestUrl.match(resetApi) || requestUrl.match(addApi)){
   body.status = "1"
   body.data = "登录成功"
-  body.type = "9"
+  body.type = temp.type
   body.token = params.token
   body.email = temp.email
   body.appleid = temp.appleid
+  body.nickname = temp.nickname
   body.device = params.device
   body.uuid = params.uid
 }
 
 if(requestUrl.match(configApi)){
   //var keyRegex = /ONLINE_REWARDAD_CONFIG_V558|ONLINE_INTERAD_DOWNLOAD_CONFIG_V558|ONLINE_ENABLE_REWARDAD_V558|ONLINE_SHOWFULLFUNC_V558|PARAV2_SPLASHADS_CONFIG|ONLINE_IAP_VERSION/
-  let data = body.data
+  var data = body.data
   //for(let i = data.length - 1; i >= 0; i--){
   //  let cokey = data[i].cokey
   //  if(!cokey.match(keyRegex)){
