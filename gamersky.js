cfc50062216d1fa302b042561de5389d06267bc8
yang <joker_yang@foxmail.com>
2024-11-10 09:20:53 +0800

    更新 gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -4,7 +4,7 @@ var channels = body.channels;
 
 //新闻
 if(listElements){
-  var titleRegex = /艾什莉体模|碍事梨体模|东北雨姐|小美人鱼|美人鱼哈莉|黑神话|周鸿祎|进击的巨人|三笠|尘白禁区|王冰冰|郭敬明|绝区零|哈莉*贝利|鸣潮|余承东|问界|鸿蒙|DNF|dnf|逆水寒|贾跃亭|侃爷|赞达亚|原神|星穹铁道/;
+  var titleRegex = /羊蹄山|艾什莉体模|碍事梨体模|东北雨姐|小美人鱼|美人鱼哈莉|黑神话|周鸿祎|进击的巨人|三笠|尘白禁区|王冰冰|郭敬明|绝区零|哈莉*贝利|鸣潮|余承东|问界|鸿蒙|DNF|dnf|逆水寒|贾跃亭|侃爷|赞达亚|原神|星穹铁道/;
   var typeRegex = /huanDeng|hengFu/;
   for (let i = listElements.length - 1; i >= 0; i--) {
     let temp = listElements[i]
