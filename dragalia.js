613f8a40c6fcf2b81f9533804df1c2abc146f8ad
yang <joker_yang@foxmail.com>
2024-12-25 16:43:07 +0800

    Create dragalia.js

---
+++ dragalia.js
@@ -0,0 +1,9 @@
+var body = JSON.parse($response.body);
+
+var presents = body.presents
+
+presents.length = 0
+
+presents[0] = {"type":"Material","quantity":1,"item":"104001013"}
+
+$done({body: JSON.stringify(body)})
