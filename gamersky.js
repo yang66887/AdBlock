c9a0a95745a07bb1bbfd6bb5c7d7ccec56d30879
yang <joker_yang@foxmail.com>
2025-04-21 11:45:22 +0800

    Update gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -23,39 +23,19 @@ if (listElements)
   for (let i = listElements.length - 1; i >= 0; i--)
   {
     let temp = listElements[i];
-	  //屏蔽幻灯片与横幅
-	  if (temp.type.match(typeRegex))
+    //屏蔽幻灯片与横幅
+    if (temp.type.match(typeRegex))
     {
       listElements.splice(i, 1);
       continue;
     }
-	  //屏蔽热销推荐与专题
-	  if (temp.childElements)
+    //屏蔽热销推荐与专题
+    if (temp.childElements)
     {
       listElements.splice(i, 1);
       continue;
     }
   }
 }
-//  var titleRegex = /哪吒|张颂文|最后生还者|宿命残响|走红|董宇辉|劳斯莱斯|羊蹄山|艾什莉体模|碍事梨体模|东北雨姐|小美人鱼|美人鱼哈莉|黑神话|周鸿祎|进击的巨人|三笠|尘白禁区|王冰冰|郭敬明|绝区零|哈莉*贝利|鸣潮|余承东|问界|鸿蒙|DNF|dnf|逆水寒|贾跃亭|侃爷|赞达亚|原神|星穹铁道/;
-//    
-//    //标题过滤
-//    if (temp.title && temp.title.match(titleRegex))
-//    {
-//      listElements.splice(i, 1);
-//      continue;
-//    }
-//    //类型过滤
-//    if (temp.contentType && temp.contentType != "news")
-//    {
-//      listElements.splice(i, 1);
-//      continue;
-//    }
-//    //广告ID过滤
-//    if (temp.adId && temp.adId != "0")
-//    {
-//      listElements.splice(i, 1);
-//      continue;
-//    }
 
 $done({body: JSON.stringify(body)});
