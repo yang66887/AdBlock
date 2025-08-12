1a151baca21bf1bda3daf2e232f60bcffe601bc1
yang <joker_yang@foxmail.com>
2025-08-12 10:17:35 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -1,5 +1,5 @@
 var body = JSON.parse($response.body);
 
-body = {"success": true, "code": 0, "data": {}, "msg": "success"}
+body = {"success": true, "code": 0, "data": false, "msg": "success"}
 
 $done({body: JSON.stringify(body)});
