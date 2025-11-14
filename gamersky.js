e29686987574248b4eeeeae90876b3340118e50f
yang <joker_yang@foxmail.com>
2025-11-14 11:27:56 +0800

    更新 gamersky.js

--- gamersky.js
+++ gamersky.js
@@ -19,11 +19,18 @@ if (channels)
 //新闻
 if (listElements)
 {
+  var ptcRegex = /社区交流/;
   var typeRegex = /huanDeng|hengFu|tieZi/;
   var titleRegex = /王自如|faker|Faker|董明珠|女友|剑星|韩援|王晶|向太|顽皮狗|李小璐|贾乃亮|甜馨|叶珂|黄晓明|明末|labubu|LABUBU|泡泡玛特|Labubu|拉布布|王宁|孟羽童|远征33|光与影|33.*远征|乔尔|最后生还者|刘浩存|哪吒|张颂文|美末|宿命残响|走红|董宇辉|劳斯莱斯|羊蹄山|艾什莉体模|碍事梨体模|东北雨姐|小美人鱼|美人鱼哈莉|黑神话|周鸿祎|进击的巨人|三笠|尘白禁区|王冰冰|郭敬明|绝区零|哈莉*贝利|鸣潮|余承东|问界|鸿蒙|DNF|dnf|逆水寒|贾跃亭|侃爷|赞达亚|原神|星穹铁道/;
   for (let i = listElements.length - 1; i >= 0; i--)
   {
     let temp = listElements[i];
+    //社区交流过滤
+    if (temp.publishTimeCaption && temp.publishTimeCaption.match(ptcRegex))
+    {
+      listElements.splice(i, 1);
+      continue;
+    }
     //标题过滤
     if (temp.title && temp.title.match(titleRegex))
     {
